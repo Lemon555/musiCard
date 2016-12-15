44118ba02dce316af89d35b8efe8c3b1a4e48ccc
Martin Hsia <marandy0905@gmail.com>
2016-12-15 12:31:58 +0800

    Rubocop Checked

--- models/song.rb
+++ models/song.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
-
-Song = Struct.new :search_id, :track_name, :track_id, :artists, :album, :link, :images
+Song = Struct.new :search_id, :track_name, :track_id, :artists, :album, :link,
+                  :images
