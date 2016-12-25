6b24c282926b1699d2f7ed0924088cf0102ce1b4
Elisa Wang <elisa.wang@iss.nthu.edu.tw>
2016-12-25 16:15:49 +0800

    multi-searching without concurrency

--- services/get_searchapi_db.rb
+++ services/get_searchapi_db.rb
@@ -3,11 +3,41 @@
 # Gets list of all songs of a search from API
 class SearchAPIdb
   extend Dry::Monads::Either::Mixin
+  extend Dry::Container::Mixin
 
   def self.call(input)
-    results = HTTP.get("#{Musicard.config.SPOTIFYSEARCH_API}/#{input}")
-    Right(SongsRepresenter.new(Songs.new).from_json(results.body))
-  rescue
-    Left(Error.new('Our servers failed - we are investigating!'))
+    Dry.Transaction(container: self) do
+      step :split_sentence
+      step :call_api_to_get_existed_songs
+    end.call(input)
   end
+
+  register :split_sentence, lambda { |input|
+    begin
+      words = []
+      if input.include? '%20'
+        words = input.split('%20')
+        words.unshift(input)
+        # unshift will add a new item to the beginning of an array.
+      else
+        words.push(input)
+      end
+      Right(words)
+    rescue
+      Left(Error.new('Failed to split the input sentence!'))
+    end
+  }
+
+  register :call_api_to_get_existed_songs, lambda { |search_terms|
+    begin
+      songs_arr = []
+      search_terms.each do |word|
+        result = HTTP.get("#{Musicard.config.SPOTIFYSEARCH_API}/#{word}")
+        songs_arr.push(SongsRepresenter.new(Songs.new).from_json(result.body))
+      end
+      Right(songs_arr)
+    rescue
+      Left(Error.new('(get) Our servers failed - we are investigating!'))
+    end
+  }
 end
